98bfcc218963a1c5a113569a91f85fb8b4fa9d06
Andrew Fernandez <bhsdrew@imac.home>
2013-11-03 16:17:00 -0500

    added foundation and initial styles

--- Gemfile
+++ Gemfile
@@ -4,5 +4,7 @@ ruby "1.9.3"
 gem 'sinatra', '1.4.4'
 gem 'unicorn'
 gem 'sinatra-assetpack', :require => 'sinatra/assetpack'
+gem 'sinatra-support', :require => 'sinatra/support' 
+gem 'compass'
 gem 'sass'
 gem 'uglifier'
--- lib/deviate.rb
+++ lib/deviate.rb
@@ -3,33 +3,40 @@
 require "rubygems"
 require "sinatra/base"
 require 'sinatra/assetpack'
+require 'sinatra/support'
+require 'compass'
 
 
 class Deviate < Sinatra::Application
 	 set :root, File.expand_path("../", File.dirname(__FILE__))
      set :views, File.expand_path("../", File.dirname(__FILE__)) + '/lib/views'
+     set :scss, { :load_paths => [ "public/sass" ] }
 
 	register Sinatra::AssetPack
+	register Sinatra::CompassSupport
 
-	  assets {
+	  assets do
 	    serve '/js',     from: 'public/js'        # Default
-	    serve '/css',    from: 'public/css'       # Default
+	    serve '/css',    from: 'public/sass'       # Default
 	    serve '/images', from: 'public/images'    # Default
+	    serve '/fonts', from: 'public/fonts'    # Default
 
 	    # The second parameter defines where the compressed version will be served.
 	    # (Note: that parameter is optional, AssetPack will figure it out.)
-	    js :app, '/js/app.js', [
-	      '/js/vendor/**/*.js',
-	      '/js/lib/**/*.js'
+	    js :main, '/js/deviate.js', [
+	      '/js/vendor/foundation/foundation.js'
 	    ]
 
-	    css :application, '/css/application.css', [
-	      '/css/screen.css'
+	    css :main, '/css/deviate.css', [
+	      '/css/normalize.css',
+	      '/css/foundation.css',
+	      '/css/main.css'
 	    ]
 
 	    js_compression  :uglify    # :jsmin | :yui | :closure | :uglify
 	    css_compression :sass   # :simple | :sass | :yui | :sqwish
-	  }
+	    prebuild true 
+	  end
 
 
 
