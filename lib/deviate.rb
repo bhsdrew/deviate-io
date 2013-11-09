dd1b82655f34ab3756591607a044025103d85108
Andrew Fernandez <bshdrew@gmail.com>
2013-11-09 16:06:11 -0500

    remove deployer

--- lib/deviate.rb
+++ lib/deviate.rb
@@ -6,7 +6,6 @@ require 'sinatra/assetpack'
 require 'sinatra/support'
 require 'compass'
 require 'minigit'
-require_relative 'init'
 
 class Deviate < Sinatra::Application
 	 set :root, File.expand_path("../", File.dirname(__FILE__))
@@ -75,38 +74,5 @@ class Deviate < Sinatra::Application
   		update = MiniGit::Capturing.pull
   		update
   end
-
-
-  #git hooks
-  get '/public_key' do
-  ::CURRENT_SSH_KEY
-end
-
-get '/status' do
-  c = GitPusher.local_state(ENV['GITHUB_REPO'])
-  "SHA: #{c.sha} | Date: #{c.date}"
-end
-
-get '/nuke-repos' do
-  `rm -r repos`
-  "nuked!"
-end
-
-get '/force-push' do
-  GitPusher.deploy(ENV['GITHUB_REPO'])
-  "Success!"
-end
-
-post '/post-receive' do
-  data = JSON.parse(params[:payload])
-  # if data["repository"]["private"]
-  #   "freak out"
-  # end
-  url = data["repository"]["url"]
-  GitPusher.deploy(url)
-  "Success!"
-end
-
-
 
 end
