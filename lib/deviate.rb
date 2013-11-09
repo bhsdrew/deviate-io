7fad2bedb14e21e51dd3c62e185b9e290e258d78
Andrew Fernandez <bshdrew@gmail.com>
2013-11-09 15:57:36 -0500

    add hooks

--- lib/deviate.rb
+++ lib/deviate.rb
@@ -6,6 +6,8 @@ require 'sinatra/assetpack'
 require 'sinatra/support'
 require 'compass'
 require 'minigit'
+require 'git'
+require 'heroku'
 
 class Deviate < Sinatra::Application
 	 set :root, File.expand_path("../", File.dirname(__FILE__))
@@ -74,5 +76,38 @@ class Deviate < Sinatra::Application
   		update = MiniGit::Capturing.pull
   		update
   end
+
+
+  #git hooks
+  get '/public_key' do
+  ::CURRENT_SSH_KEY
+end
+
+get '/status' do
+  c = GitPusher.local_state(ENV['GITHUB_REPO'])
+  "SHA: #{c.sha} | Date: #{c.date}"
+end
+
+get '/nuke-repos' do
+  `rm -r repos`
+  "nuked!"
+end
+
+get '/force-push' do
+  GitPusher.deploy(ENV['GITHUB_REPO'])
+  "Success!"
+end
+
+post '/post-receive' do
+  data = JSON.parse(params[:payload])
+  # if data["repository"]["private"]
+  #   "freak out"
+  # end
+  url = data["repository"]["url"]
+  GitPusher.deploy(url)
+  "Success!"
+end
+
+require_relative 'lib/init'
 
 end
