af8ad8ce2fdf0436285b32d61be3628abeaea40a
Andrew Fernandez <bshdrew@gmail.com>
2013-11-10 11:55:01 -0500

    deploy automation

--- lib/heroku.rb
+++ lib/heroku.rb
@@ -1,14 +1,14 @@
-if ENV['I_AM_HEROKU']
-  Heroku::Auth.instance_eval do
-    # Autologin
-    @credentials = ['bhsdrew', 'fcca56b1-d3c2-4fdb-9b44-d0e9e1fb1233']
-    # Set up keys if there aren't any
-    key_path = "#{home_directory}/.ssh/id_rsa.pub"
-    if available_ssh_public_keys.empty?
-      puts "Generating New Pair"
-      generate_ssh_key("id_rsa")
-      associate_key(key_path)
-    end
-    ::CURRENT_SSH_KEY = File.read(key_path)
-  end
-end
+#if ENV['I_AM_HEROKU']
+#  Heroku::Auth.instance_eval do
+#    # Autologin
+#    @credentials = ['bhsdrew', 'fcca56b1-d3c2-4fdb-9b44-d0e9e1fb1233']
+#    # Set up keys if there aren't any
+#    key_path = "#{home_directory}/.ssh/id_rsa.pub"
+#    if available_ssh_public_keys.empty?
+##      puts "Generating New Pair"
+#      generate_ssh_key("id_rsa")
+#      associate_key(key_path)
+#    end
+#    ::CURRENT_SSH_KEY = File.read(key_path)
+#  end
+#end
